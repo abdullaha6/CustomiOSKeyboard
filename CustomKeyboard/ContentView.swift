d8f84c4025ccc798fc34160cd8bb9774c5ea114b
Abdullah Ajmal <abdullaha6@gmail.com>
2023-12-22 19:56:28 +0530

    Initial Commit

---
+++ CustomKeyboard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CustomKeyboard
+//
+//  Created by Abdullah Ajmal on 2023-12-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CustomKeyboard/CustomKeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomKeyboardApp.swift
+//  CustomKeyboard
+//
+//  Created by Abdullah Ajmal on 2023-12-22.
+//
+
+import SwiftUI
+
+@main
+struct CustomKeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
